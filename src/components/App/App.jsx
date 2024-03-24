65d555e43bd5ddb24e14bfc7c4d54976834ecabe
talamus1972 <talamus1972@gmail.com>
2024-03-24 10:05:16 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,14 @@
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactForm from "../ContactForm/ContactForm";
-
+import Loader from "../Loader/Loader"
 
 export default function App() {
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
+      <Loader />      
       <SearchBox />
       <ContactList />
     </div>
